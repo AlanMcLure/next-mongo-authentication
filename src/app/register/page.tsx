22162fd41121d435dbc344feff26523a9a5d10e0
AlanMcLure <alanmclurealarcon02@gmail.com>
2023-12-20 22:58:11 +0100

    Signup form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,87 @@
-function RegisterPage() {
+"use client";
+import { FormEvent, useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+function Signup() {
+    const [error, setError] = useState<string | undefined>();
+    const router = useRouter();
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        try {
+            const formData = new FormData(event.currentTarget);
+            const signupResponse = await fetch("/api/auth/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: formData.get("email"),
+                    password: formData.get("password"),
+                    fullname: formData.get("fullname"),
+                }),
+            });
+
+            if (!signupResponse.ok) {
+                throw new Error(`Signup request failed with status ${signupResponse.status}`);
+            }
+
+            const signupData = await signupResponse.json();
+            console.log(signupData);
+
+            const res = await signIn("credentials", {
+                email: signupData.email,
+                password: formData.get("password"),
+                redirect: false,
+            });
+
+            if (res?.ok) {
+                return router.push("/dashboard/profile");
+            }
+        } catch (error) {
+            console.error(error);
+            setError("Error signing up. Please try again."); // Generic error message
+        }
+    };
+
     return (
-        <div>RegisterPage</div>
-    )
+        <div className="justify-center h-[calc(100vh-4rem)] flex items-center">
+            <form onSubmit={handleSubmit} className="bg-neutral-950 px-8 py-10 w-3/12">
+                {error && <div className="bg-red-500 text-white p-2 mb-2">{error}</div>}
+                <h1 className="text-4xl font-bold mb-7">Signup</h1>
+
+                <label className="text-slate-300">Fullname:</label>
+                <input
+                    type="text"
+                    placeholder="Fullname"
+                    className="bg-zinc-800 px-4 py-2 block mb-2 w-full"
+                    name="fullname"
+                />
+
+                <label className="text-slate-300">Email:</label>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    className="bg-zinc-800 px-4 py-2 block mb-2 w-full"
+                    name="email"
+                />
+
+                <label className="text-slate-300">Password:</label>
+                <input
+                    type="password"
+                    placeholder="Password"
+                    className="bg-zinc-800 px-4 py-2 block mb-2 w-full"
+                    name="password"
+                />
+
+                <button className="bg-blue-500 text-white px-4 py-2 block w-full mt-4">
+                    Signup
+                </button>
+            </form>
+        </div>
+    );
 }
 
-export default RegisterPage
+export default Signup;
